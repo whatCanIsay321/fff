a8f46c56d62bcbce4505dc85b40ac15ffbaf3ddf
tjj <3419489648@qq.com>
2025-08-02 16:10:33 +0800

    第一次提交

---
+++ main.py
@@ -0,0 +1,16 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+if __name__ == "__main__":
+    # host="0.0.0.0" 允许外部访问，port=8000 是默认端口
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
